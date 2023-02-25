b61fd73c6cacbf0b9116b7376c49cc1c87d65f7b
Alibek Beket <alibek1beket@gmail.com>
2023-02-25 13:58:11 +0600

    стилизовал главную страницу и добавил иконки в ссылки

--- app/guest_book/views/guest_book.py
+++ app/guest_book/views/guest_book.py
@@ -3,7 +3,7 @@ from guest_book.models import GuestBook
 
 
 def guest_book_view(request):
-    guest_book_list = GuestBook.objects.all()
+    guest_book_list = GuestBook.objects.filter(status='active').order_by('-created_at')
     context = {
         'guest_book_list': guest_book_list
     }
