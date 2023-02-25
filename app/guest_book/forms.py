99b8c4f038dfbfb045313f8228d8cb6483848d25
Alibek Beket <alibek1beket@gmail.com>
2023-02-25 14:54:38 +0600

    Создал класс формы, частичные шаблоны и функцию добавления записи. Успешно внедрил его в домашнюю страницу после списка и стилизовал его

---
+++ app/guest_book/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from guest_book.models import GuestBook
+
+
+class GuestBookForm(forms.ModelForm):
+    class Meta:
+        model = GuestBook
+        fields = ("author_name", "author_email", "text")
+        labels = {
+            'author_name': 'Имя',
+            'author_email': 'Email',
+            'text': 'Текст'
+        }
+
+    def clean_author_name(self):
+        author_name = self.cleaned_data.get('author_name')
+        if len(author_name) < 3:
+            raise ValidationError('Имя не может состоять из 1 или 2 символов')
+        return author_name
+
+    def clean_author_email(self):
+        author_email = self.cleaned_data.get('author_email')
+        if len(author_email) < 3:
+            raise ValidationError('Почта не может состоять из 1 или 2 символов')
+        return author_email
+
+    def clean_text(self):
+        text = self.cleaned_data.get('text')
+        if len(text) < 5:
+            raise ValidationError('Текст не может состоять из 1 или 2 символов')
+        return text
--- app/guest_book/views/guest_book.py
+++ app/guest_book/views/guest_book.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from guest_book.models import GuestBook
+
+from guest_book.forms import GuestBookForm
 
 
 def guest_book_view(request):
     guest_book_list = GuestBook.objects.filter(status='active').order_by('-created_at')
-    context = {
-        'guest_book_list': guest_book_list
-    }
-    return render(request, 'guest_book_list_page.html', context=context)
+    if not request.POST:
+        form = GuestBookForm()
+        context = {
+            'guest_book_list': guest_book_list,
+            'form': form
+        }
+        return render(request, 'guest_book_list_page.html', context=context)
+    form = GuestBookForm(data=request.POST)
+    if not form.is_valid():
+        context = {
+            'guest_book_list': guest_book_list,
+            'form': form
+        }
+        return render(request, 'guest_book_list_page.html', context=context)
+    else:
+        GuestBook.objects.create(**form.cleaned_data)
+        return redirect(reverse('guest_book_list'))
